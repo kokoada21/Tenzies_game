36458bd7b5897844d03a2c94f3879388404f928d
kokoada21 <adamjesina@gmail.com>
2023-08-15 17:05:58 +0200

    finished highscore saving and loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,29 @@
-import { useState, useEffect }  from 'react'
+import { useState, useEffect, useRef }  from 'react'
 import './App.css'
 import Die from './components/Die'
 import { nanoid } from 'nanoid'
+import Confetti from 'react-confetti'
 
 function App() {
+  const numberOfDice = 10;
+  const intervalRef = useRef();
+
   const [diceValues, setDiceValues] = useState(allNewDice())
-  const [won, setWon] = useState(false);
+  const [rolls, setRolls] = useState(0);
+  const [time, setTime] = useState(0);
+  const [gameStatus, setGameStatus] = useState("start");
+  const [highscore, setHighscore] = useState({});
+  
 
-
-  useEffect(() =>{
-    const allHeld = diceValues.every(die => die.isHeld);
-    const firstValue = diceValues[0].value;
-    const allSameValue = diceValues.every(die => die.value === firstValue)
-    if(allHeld && allSameValue) {
-      setWon(true);
-      console.log("You won");
-    }
-  },[diceValues])
+  useEffect(() => {
+    if("highscore" in localStorage){
+       setHighscore(JSON.parse(localStorage.getItem("highscore")));
+      }
+  }, [gameStatus])
 
   function allNewDice() {
     const newDice = [];
-    for(let i = 0; i < 10; i++){
+    for(let i = 0; i < numberOfDice; i++){
       newDice.push(generateNewDie());
     }
     return newDice
@@ -34,16 +37,29 @@ function App() {
     }
   }
 
-  function rollDice() {
+  function play() {
+    if(gameStatus =="start") {
+      setGameStatus("playing")
+      rollDice()
+    }else if(gameStatus == "playing") {
+      rollDice()
+    }else {
+      resetGame()
+    }
+  }
+
+  function rollDice(){
     setDiceValues(prevDice => 
-        prevDice.map(die => {
-          return die.isHeld ?
-            die :
-            generateNewDie()
-        }))
+      prevDice.map(die => {
+        return die.isHeld ?
+          die :
+          generateNewDie()
+      }))
+    setRolls(rolls => rolls + 1)
   }
 
   function holdDice(id) {
+    if(gameStatus === "start") setGameStatus("playing")
     setDiceValues((prevDicevalues) => 
        prevDicevalues.map((prevDie) => {
         if(prevDie.id === id){
@@ -56,7 +72,47 @@ function App() {
         }
       }))
   }
-  
+  //Timer setup
+  useEffect(() => {
+      if(gameStatus === "playing"){
+        intervalRef.current = setInterval(() => {
+          setTime(time => time +1)
+        }, 1000);
+      }
+      return () => clearInterval(intervalRef.current);
+  }, [gameStatus])
+
+  //Winning condition
+  useEffect(() =>{
+    const allHeld = diceValues.every(die => die.isHeld);
+    const firstValue = diceValues[0].value;
+    const allSameValue = diceValues.every(die => die.value === firstValue)
+    if(allHeld && allSameValue) {
+      pushHighscore();
+      setGameStatus("won");
+    }
+  },[diceValues])
+
+  function pushHighscore(){
+    let currectHighscore;
+    if(JSON.stringify(highscore) === "{}"){
+      currectHighscore = {rolls: rolls, time: time}
+      console.log("isEmptu pco")
+    }else{
+      currectHighscore = {
+        rolls: rolls < highscore.rolls ? rolls : highscore.rolls,
+        time: time < highscore.time ? time : highscore.time
+      }
+    }
+    localStorage.setItem("highscore", JSON.stringify(currectHighscore));
+  }
+
+  function resetGame() {
+    setGameStatus("start")
+    setRolls(0);
+    setTime(0);
+    setDiceValues(allNewDice()); 
+  }
 
   const diceElement = diceValues.map(die => 
       <Die 
@@ -65,15 +121,28 @@ function App() {
       isHeld={die.isHeld}
       holdDice={() => holdDice(die.id)}
       />)
-
+  
   return (
-    <main className='main--container'>
+    <main>
+    <section className='main--container'>
+      {gameStatus==="won" && <Confetti/>}
       <h1>Tenzies</h1>
       <h4>Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</h4>
       <div className='dies--container'>
         {diceElement}
      </div>
-      <button onClick={rollDice}>Roll</button>
+      <button
+        className='roll--button' 
+        onClick={play}>
+        {gameStatus=="won" ? "New game" : "Roll"}
+      </button>
+    </section>
+    <section className='stats--panel'>
+      <h4>Rolls: {rolls}</h4>
+      <h4>Time: {time}</h4>
+      <h4>Min rolls: {highscore.rolls}</h4>
+      <h4>Best time: {highscore.time}</h4>
+    </section>
     </main>
   )
 }
